538920ef4597906aa5fc33dc396bce7c137e515c
CiudadAlex <ciudad.alex@gmail.com>
2024-01-04 01:39:46 +0100

    Initial commit

--- TextSteganography.py
+++ TextSteganography.py
@@ -6,11 +6,28 @@ MASK_WORD = "<mask>"
 class TextSteganography:
 
     def __init__(self):
-        self.maskFiller = pipeline("fill-mask", model="distilroberta-base")
+        self.mask_filler = pipeline("fill-mask", model="distilroberta-base")
+        self.text_generator = pipeline("text-generation", model="gpt2", max_new_tokens=200)
+        self.final_padding = 30
 
-    def hide_message(self, list_tuples_word_position):
-        output = self.maskFiller("Paris is the " + MASK_WORD + " of France.")
-        return output[0]
+    def hide_message(self, list_word, list_position, subject):
+
+        if len(list_word) != len(list_position):
+            raise Exception()
+
+        num_words = max(list_position) + self.final_padding
+
+        text = subject
+        words = text.split()
+
+        while len(words) < num_words:
+            output_text_generator = self.text_generator(text)
+
+            text = output_text_generator[0]['generated_text']
+            words = text.split()
+
+        output = self.mask_filler("Paris es la " + MASK_WORD + " de Francia.")
+        return text
 
 
 '''
--- main.py
+++ main.py
@@ -2,7 +2,33 @@ from TextSteganography import TextSteganography
 
 textSteganography = TextSteganography()
 
-list_tuples_word_position = None
-output = textSteganography.hide_message(list_tuples_word_position)
+list_word = [
+    'Paris',
+    'is',
+    'in',
+    'danger',
+    'send',
+    'the',
+    'planes'
+]
 
+list_position = [
+    4,
+    25,
+    32,
+    55,
+    62,
+    78,
+    92
+]
+subject = "The number 42"
+output = textSteganography.hide_message(list_word, list_position, subject)
+
+print("___________________________________________")
 print(output)
+print("___________________________________________")
+
+words = output.split()
+
+for index in list_position:
+    print(words[index])
